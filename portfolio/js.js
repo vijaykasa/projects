051736718e77ef631e1fc3801c168e9b9baa8d85
vijaykasa <kasavijaykumar@errortech.in>
2023-04-06 19:13:08 +0530

    first commit

--- portfolio/js.js
+++ portfolio/js.js
@@ -1,11 +1,31 @@
 let menu = document.getElementById("menu")
 let menu_bar= document.getElementById("menu_bar")
+let home_left=document.getElementById("home_left")
+let btn=document.getElementById("btn")
+let li=document.querySelectorAll("li a")
+console.log(li[0])
 
 menu.addEventListener("click", ()=>{
-    console.log("work")
     menu_bar.classList.toggle("active")
 })
 
 window.onscroll=()=>{
     menu_bar.classList.remove("active")
 }
+window.onload=()=>{
+    home_left.classList.add("show")
+    setTimeout( btn.classList.add("slidup"),1000)   
+}
+
+li.forEach(Element=>{
+   
+    Element.addEventListener("click",(e)=>{
+        li.forEach(ele=>ele.classList.remove("active"))
+        e.target.classList.add("active")
+    })
+})
+
+
+
+
+
